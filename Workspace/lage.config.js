8decd0d1e2a98e0c75d9d2a59513dd1a7cdd1025
Danny van Velzen 🁴 <dannyvv@microsoft.com>
2025-01-28 14:06:19 -0800

    Add sample

---
+++ Workspace/lage.config.js
@@ -0,0 +1,23 @@
+
+export default {
+    extends: "../lage.config.js",
+    pipelines: {
+        // inherits the build and bundle from common settings, but at this layer we are adding test support.
+        // in reality we'll share test and lint, just illustrative :)
+        test: {
+            dependsOn: ["build"],
+            inputs: "src/test/**",
+        },
+        lint: {
+            inputs: ["src/**", ".eslintrc.js", "{workspaceRoot}/.eslintrc.js", "{workspaceRoot}/prettier.config.js" ], // this repo uses shared prettier config 
+            outputs: ["logs/**"], // Lint should only have outputs.
+            options: {
+                "untrackedFolder": "node_modules/.cache", // Tell BuildXL to ignore the node_modules/.cache folder because we will place our trust in eslints incrementality
+                "environment": {
+                    // set environment variable so that eslint's cache is forced into the shared untracked cache folder.
+                    "ESLINT_CACHE": "node_modules/.cache/eslint"
+                }
+            }
+        }
+    }
+}
---
+++ lage.config.js
@@ -0,0 +1,24 @@
+export default {
+    pipelines: {
+        // undocumented feature: Allows shared defaults for all pipeline targets with *.
+        "*": {
+            options: {
+                // set some BuildXL options  for all targets
+                environment: {
+                    "SRCROOT": {passthrough: true} // Tells BuildXL to take the SRCROOT variable as-is and pass it to the child process.
+                    "BUILDROOT": {passthrough: true} // Tells BuildXL to take the SRCROOT variable as-is and pass it to the child process.
+                }
+            }
+        },
+        build: {
+            dependsOn: ["^build"],
+            inputs: ["src/**" ],
+            outputs: ["lib/**", "logs/**" ] // all build outputs go to lib folder and all detailed log files go to logs folder
+        },
+        bundle: {
+            dependsOn: ["build"],
+            inputs: ["src/**", "assets/**", "^lib/**"], // Uses all source files and all asset files as well as the generated files from build verb
+            outputs: ["dist/**", "logs/**"] // all bundle outputs go to dist folder and all detailed log files go to logs folder
+        }
+    }
+}
